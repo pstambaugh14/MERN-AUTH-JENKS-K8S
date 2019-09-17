231d0397acff722d655ef8c74061e72072b7c3da
pstambaugh14 <pstambaugh14@gmail.com>
2019-09-17 16:31:27 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://user:Vegita1234@cluster0-z2oqe.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "559f151a-ce65-41d8-b81b-e163cb1953ab"
+  mongoURI: "mongodb+srv://user:<password>@cluster0-z2oqe.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
