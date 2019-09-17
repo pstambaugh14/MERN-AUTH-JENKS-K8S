5dbc61c81012880f4918e9a6fab0df22c9f311b9
Patrick Stambaugh <pstambaugh14@gmail.com>
2019-09-17 16:26:00 -0400

    git git

--- registry4.sh
+++ registry4.sh
@@ -2,5 +2,4 @@ docker run -d -p 5001:5001 --restart=always --name registry \
   -v /certs:/certs \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
-  -e HTTP_PROXY=http://127.0.0.1:3001
   registry:2
