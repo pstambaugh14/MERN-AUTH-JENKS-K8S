f3f2118f884107ebd9d41fbf2d439a8cfdbd09a4
Patrick Stambaugh <pstambaugh14@gmail.com>
2019-09-21 16:52:20 -0400

    keys should be encrypted eventually...

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://user:<password>@cluster",
-  secretOrKey: "secret-or-key"
+  mongoURI: "mongodb+srv://user:Vegita1234@cluster0-z2oqe.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "559f151a-ce65-41d8-b81b-e163cb1953ab"
 };
