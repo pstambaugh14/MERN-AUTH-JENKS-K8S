30481be5e9799d8fdd5ecf901a528558ddf731f2
Patrick Stambaugh <pstambaugh14@gmail.com>
2019-09-25 04:59:54 -0400

    removal of excess

--- registry2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d -p 5001:5001 --restart=always --name registry -v /home/patrick/Apps/MERN-AUTH-JENKS-K8S/registry -e REGISTRY_HTTP_ADDR=0.0.0.0:5001 -e REGISTRY_HTTP_HOST=192.168.0.158 -e REGISTRY_HTTP_TLS_CERTIFICATE=/home/patrick/Apps/MERN-AUTH-JENKS-K8S/registry/cert.crt -e REGISTRY_HTTP_TLS_KEY=/home/patrick/Apps/MERN-AUTH-JENKS-K8S/registry/key.key registry:latest
